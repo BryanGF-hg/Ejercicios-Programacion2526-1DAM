8193c52bc4e32ec53b555c8f8b7f031e1ebb8903
BryanGF-hg <bryan.alexander.glot.fong@alu.ceacfp.es>
2025-09-23 09:57:01 +0200

    Create 001- variables ciclismo.py

---
+++ 001- variables ciclismo.py
@@ -0,0 +1,52 @@
+#Ejercicio
+
+nombre_ciclista = input("Dame tu nombre: ")
+distancia_recorrida = input("Dime cuanto kilómetros has hecho en tu último recorrido: ")
+
+print("El ciclista se llama",nombre_ciclista,"y tu último recorrido ha hecho",distancia_recorrida,"kilómetros")
+
+
+## Explicación (en Markdown)
+
+#Práctica con Variables y salidas
+
+En esta práctica usaremos variables y salidas para el fin de poder afianzar en estos 2 conceptos básicos que nos ayudaran a sentar base.
+
+Esta práctica, nos permite ver que las variables y las salidas son un medio para un fin, lo mismo, como en completar una ruta o entrenar por meses.
+
+#Requisitos:
+
+ -Declara variable nombre_ciclista
+ -Declara variable distancia_recorrida
+ -Usar función print() para mostrar un mensaje con nombre_ciclista
+ -También distancia_recorrida
+
+
+Primero definimos nuestras variables, "nombre-ciclista" como una cadena de texto y "distancia_recorrida" como un número entero:
+
+```
+nombre_ciclista = Bryan
+
+distancia_recorrida = 50
+```
+
+Luego mostramos por pantalla los resultados en un mensaje completo:
+
+```
+print("El ciclista se llama",nombre_ciclista,"y tu último recorrido ha hecho",distancia_recorrida,"kilómetros")
+```
+
+Algunos errores que podemos encontrar son: no definir una variable como se debe, es decir, nombreciclista en vez de nombre_ciclista; otro error puede ser olvidar comas para definir una variable dentro de un mensaje de texto
+
+#Conclusión:
+
+En conclusión, al definir variables nos permite hacer operaciones básicas o complejas según las instrucciones dadas y mostrarlo en pantalla nos ayuda a saber si las variables corresponden a los valores asignados. Es una herramienta útil y eficiente tanto a nivel sencillo como a nivel complejo
+
+```
+nombre_ciclista = Bryan
+distancia_recorrida = 50
+
+print("El ciclista se llama",nombre_ciclista,"y tu último recorrido ha hecho",distancia_recorrida,"kilómetros")
+```
+
+
