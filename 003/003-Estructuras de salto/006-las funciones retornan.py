b34e110b2513bdeeb365effe98c1d2f8063bc716
Hiro <bryan.alexander.glot.fong@alu.ceacfp.es>
2025-10-03 10:23:08 +0200

    Add files via upload

---
+++ 003/003-Estructuras de salto/006-las funciones retornan.py
@@ -0,0 +1,11 @@
+#Deben escribirse con camelCase
+#Deben tener un verbo (infinito o imperativo) y un objecto directo
+#Deben tener un nombre descriptivo
+#Parametro es el valor que entra en la función
+#Return es la forma limpia de sacar información de una función
+
+def diHola(nombre,edad):
+ return "Hola"+nombre+",tienes"+str(edad)+"años y yo te saludo"
+ 
+diHola("Jose Vicente",47) 
+diHola("Jorge",48)
