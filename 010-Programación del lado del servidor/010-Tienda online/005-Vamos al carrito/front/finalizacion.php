ddc53372f93b0525bdd9c6f00a76a5327f7a220b
Bryan <bryanglot@hotmail.com>
2025-12-15 10:19:44 +0100

    5

---
+++ 010-Programación del lado del servidor/010-Tienda online/005-Vamos al carrito/front/finalizacion.php
@@ -0,0 +1,2 @@
+Esta es la página de finalizacion<br>
+<a href="index.php">Acabaste, volvemos a empezar</a>
---
+++ 010-Programación del lado del servidor/010-Tienda online/005-Vamos al carrito/front/inc/piedepagina.php
@@ -0,0 +1,15 @@
+  </main>
+  <footer>
+   <!-- Eliminar al final de "completar" el sitio web-->
+   <a href="catalogo.php">catálogo</a>
+   <a href="carrito.php">carrito</a>
+   <a href="finalizacion.php">finis</a>   
+   <a href="index.php">index</a>   
+   <a href="producto.php">producto</a>
+  </footer>
+  
+  <style>
+   footer{text-align:center;}
+  </style>
+ </body>
+</html>
